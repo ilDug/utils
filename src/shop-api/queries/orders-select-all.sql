e18fca73a0995ac3ca9fbd3f06908e000ce9d487
ilDug <dognini.marco@gmail.com>
2019-11-08 17:25:49 +0100

    order view sql

--- src/shop-api/queries/orders-select-all.sql
+++ src/shop-api/queries/orders-select-all.sql
@@ -1,4 +1,4 @@
-SELECT 
+/* SELECT 
 	JSON_SET(
 		o.order, 
 		'$.articles', 
@@ -25,26 +25,6 @@ FROM
 WHERE 
     o.uid  LIKE :uid
     
-GROUP BY o.orderId;
+GROUP BY o.orderId; */
 
-
-SELECT 
-    JSON_SET(o.order, '$.articles', a.articles_array) AS theorder
-FROM
-    orders o
-        LEFT JOIN
-    (
-		SELECT 
-			aa.orderId,
-
-            JSON_ARRAYAGG(
-                IF(aa.articleId IS NOT NULL, aa.article, NULL)
-            ) AS articles_array
-
-		FROM
-			shop.articles aa
-		GROUP BY aa.orderId
-	) a 
-        USING (orderId)
-WHERE 
-    o.uid  LIKE :uid
+# ******************************************************************** 
