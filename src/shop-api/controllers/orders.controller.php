ca9c8b294da4f72c113a62398bbc84a9926d029b
ilDug <dognini.marco@gmail.com>
2019-12-10 17:08:42 +0100

    update order api

--- src/shop-api/controllers/orders.controller.php
+++ src/shop-api/controllers/orders.controller.php
@@ -14,11 +14,19 @@ class OrdersController
     }
 
     const QF_INSERT = __DIR__ . '/../queries/order-insert.sql';
+    const QF_UPDATE = __DIR__ . '/../queries/order-update.sql';
+
 
     const FIELDS = [
         "orderId",
         "uid"
     ];
+
+
+    public function read()
+    {
+        /** TO DO fai riferimento a quello che c'è critto i products */
+    }
 
 
     /** crea un ordine nel database */
@@ -38,6 +46,26 @@ class OrdersController
 
         if (!$st->execute() || ($st->rowCount() < 1))
             throw new Exception("Errore inserimento " . json_encode($st->errorInfo()[2]), 500);
+        else return $order;
+    }
+
+    /** modifica un ordine esistente */
+    public function edit($order)
+    {
+        if (!DAG\UTILS\checkFields($order, self::FIELDS)) {
+            return false;
+        }
+
+        $order_json = json_encode($order);
+        $sql = file_get_contents(self::QF_UPDATE);
+
+        $st = $this->pdo->prepare($sql);
+        $st->bindParam(':orderId',    $order->orderId,    PDO::PARAM_STR);
+        $st->bindParam(':uid',        $order->uid,        PDO::PARAM_STR);
+        $st->bindParam(':order',      $order_json,        PDO::PARAM_STR);
+
+        if (!$st->execute())
+            throw new Exception("Errore modifica ordine " . json_encode($st->errorInfo()[2]), 500);
         else return $order;
     }
 }
---
+++ src/shop-api/queries/order-update.sql
@@ -0,0 +1,4 @@
+UPDATE orders
+SET 
+    `order` = :order
+WHERE orderId = :orderId AND uid = :uid
